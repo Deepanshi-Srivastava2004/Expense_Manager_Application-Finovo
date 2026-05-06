548d7b6671de50ab63719542989839a04aaa53ae
gluon007 <ssshrey007@gmail.com>
2026-05-06 11:40:59 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'features/widgets/Navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-<<<<<<< HEAD
 import 'src/features/screens/input_screen.dart';
-=======
 // import 'features/widgets/input_screen.dart';
->>>>>>> 673811807a20f9bc435b3c8706e77de979648358
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
